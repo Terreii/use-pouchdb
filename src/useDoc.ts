afd9b8e4f2ee6522bcb6bcd7a097673e8639fe4e
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-04-19 12:31:25 +0200

    doc(useDoc): add comments and improve types

--- src/useDoc.ts
+++ src/useDoc.ts
@@ -3,22 +3,29 @@ import { MISSING_DOC } from 'pouchdb-errors'
 
 import usePouchDB from './usePouch'
 
+/**
+ * Retrieves a document and subscribes to it's changes.
+ * @param {string} id - Document ID of the document that should be fetched.
+ * @param {object} [options] - PouchDB get options. Excluding 'open_revs'.
+ * @param {object|function} [initialValue] - Value that should be returned while fetching the doc.
+ */
 export default function useDoc<Content extends {}>(
   id: PouchDB.Core.DocumentId,
   options?: PouchDB.Core.GetOptions,
   initialValue?: (() => Content) | Content
 ) {
+  type Document = (PouchDB.Core.Document<Content> & PouchDB.Core.GetMeta) | null
+
   const pouch = usePouchDB()
 
   const { rev, revs, revs_info, conflicts, attachments, binary, latest } =
     options || {}
 
-  const [doc, setDoc] = useState<
-    Content | (PouchDB.Core.Document<Content> & PouchDB.Core.GetMeta) | null
-  >(initialValue!)
+  const [doc, setDoc] = useState<Content | Document | null>(initialValue!)
   const [state, setState] = useState<'loading' | 'done' | 'error'>('loading')
   const [error, setError] = useState<PouchDB.Core.Error | null>(null)
 
+  // Reset the document to initialValue (or null)
   const setToInitialValue = (fallBackToNull: boolean) => {
     if (initialValue && typeof initialValue === 'object') {
       setDoc(initialValue)
@@ -29,6 +36,7 @@ export default function useDoc<Content extends {}>(
     }
   }
 
+  // Reset the document if the id did change and a initial value is set.
   const lastId = useRef(id)
   useEffect(() => {
     if (id === lastId.current) return
@@ -38,6 +46,7 @@ export default function useDoc<Content extends {}>(
   }, [id, initialValue])
 
   useEffect(() => {
+    // Is this instance still current?
     let isMounted = true
 
     const fetchDoc = async () => {
@@ -70,6 +79,7 @@ export default function useDoc<Content extends {}>(
 
     fetchDoc()
 
+    // Use the changes feed to get updates to the document
     const subscription = pouch
       .changes({
         live: true,
@@ -83,6 +93,7 @@ export default function useDoc<Content extends {}>(
       .on('change', change => {
         if (!isMounted) return
 
+        // If the document got deleted it should change to an 404 error state
         if (change.deleted) {
           setToInitialValue(true)
           setError(MISSING_DOC)
@@ -103,14 +114,16 @@ export default function useDoc<Content extends {}>(
   }, [id, rev, revs, revs_info, conflicts, attachments, binary, latest])
 
   return useMemo(() => {
-    const resultDoc = doc as PouchDB.Core.Document<Content> &
-      PouchDB.Core.GetMeta
+    const resultDoc = doc as Document
+
+    // Add _id and _rev to the initial value (if they aren't set)
     if (resultDoc && resultDoc._id == null) {
       resultDoc._id = id
     }
     if (resultDoc && resultDoc._rev == null) {
       resultDoc._rev = ''
     }
+
     return {
       doc: resultDoc,
       state,

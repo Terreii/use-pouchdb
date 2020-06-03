6f63dd0f6fdc4d736237376703f4f3a0bcf76a68
Christopher Astfalk <christopher.astfalk@icloud.com>
2020-06-03 15:17:48 +0200

    chore(eslint): configure eslint-plugin-jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jest/recommended',
   ],
 }

4e06bc70dafbf6104bc3790484d0e1aeb6b44f25
Christopher Astfalk <christopher.astfalk@icloud.com>
2025-09-16 14:08:02 +0200

    chore(package): update none breaking dependencies

--- babel.config.js
+++ babel.config.js
@@ -11,8 +11,4 @@ module.exports = {
     '@babel/preset-typescript',
     '@babel/preset-react',
   ],
-  plugins: [
-    // can be removed once support for node v10 is dropped.
-    [require('@babel/plugin-proposal-class-properties'), { loose: false }],
-  ],
 }
